002a563c58636f04a094df4117db12627d64cf6d
Jan Bramkamp <crest@rlwinm.de>
2024-12-09 17:51:42 +0100

    Cleanup script

--- notify_pushover.sh
+++ notify_pushover.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -Cefu
 
-# Read configuration
+# Read configuration.
 read API_TOKEN < /etc/pushover/token
 read API_USER  < /etc/pushover/user
 read PRIORITY  < /etc/pushover/priority
@@ -12,17 +12,22 @@ SERVICE="${PAM_SERVICE:-Unknown Service}"
   RHOST="${PAM_RHOST:-Unknown Host}"
    USER="${PAM_USER:-Unknown User}"
 
-# Record hostname and time
+# Record hostname and time.
 DATE="$(date -u +'%F %T')"
 HOST="$(hostname)"
 
+# Set title and message text.
 TITLE="${SERVICE:?}: ${USER:?}@${HOST:?} from $USER@$RHOST"
 TEXT="${DATE:?}"
 
-curl -s \
+# Discard output.
+exec >/dev/null 2>&1
+
+# Become curl.
+exec curl -s \
 	-F "token=${API_TOKEN:?}" \
 	-F "user=${API_USER:?}" \
 	-F "title=${TITLE:?}" \
 	-F "message=${TEXT:?}" \
 	-F "priority=${PRIORITY:?}" \
-		https://api.pushover.net/1/messages.json >/dev/null 2>&1
+		https://api.pushover.net/1/messages.json
