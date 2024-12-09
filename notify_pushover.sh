808bcf73e3bfc9b82c4c495233d944dda96b2384
Jan Bramkamp <crest@rlwinm.de>
2024-12-09 17:42:01 +0100

    Read config from /etc/pushover/*

--- notify_pushover.sh
+++ notify_pushover.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 set -Cefu
 
-# Change these variables
-API_TOKEN='<<TOKEN>>'
-API_USER='<<USER>>'
-PRIORITY=0
+# Read configuration
+read API_TOKEN < /etc/pushover/token
+read API_USER  < /etc/pushover/user
+read PRIORITY  < /etc/pushover/priority
 
-TYPE="${PAM_TYPE:-Unknown Type}"
+# Import pam_exec environment variables.
+   TYPE="${PAM_TYPE:-Unknown Type}"
 SERVICE="${PAM_SERVICE:-Unknown Service}"
-TTY="${PAM_TTY:-Unknown TTY}"
-HOST="$(hostname)"
-RHOST="${PAM_RHOST:-Unknown Host}"
-USER="${PAM_USER:-Unknown User}"
+    TTY="${PAM_TTY:-Unknown TTY}"
+  RHOST="${PAM_RHOST:-Unknown Host}"
+   USER="${PAM_USER:-Unknown User}"
 
+# Record hostname and time
 DATE="$(date -u +'%F %T')"
+HOST="$(hostname)"
 
 TITLE="${SERVICE:?}: ${USER:?}@${HOST:?} on $TTY from $RHOST"
 TEXT="${DATE:?}: $TYPE"
 
-if [ "$PAM_TYPE" != "close_session" ]; then
-	echo curl -s \
-		-F "token=${API_TOKEN:?}" \
-		-F "user=${API_USER:?}" \
-		-F "title=${TITLE:?}" \
-		-F "message=${TEXT:?}" \
-		-F "priority=${PRIORITY:?}" \
-		https://api.pushover.net/1/messages.json # >/dev/null 2>&1
-fi
+[ "${TYPE:?}" = 'close_session' ] || curl -s \
+	-F "token=${API_TOKEN:?}" \
+	-F "user=${API_USER:?}" \
+	-F "title=${TITLE:?}" \
+	-F "message=${TEXT:?}" \
+	-F "priority=${PRIORITY:?}" \
+		https://api.pushover.net/1/messages.json >/dev/null 2>&1
